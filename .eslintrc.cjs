f409d0d1f0fbc2fc72236333b00a00a3200f7aec
Aponia <brianmv@uci.edu>
2023-08-11 04:29:14 -0700

    feat: :tada: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+const config = {
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["react", "@typescript-eslint", "simple-import-sort", "tailwindcss"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
+  ignorePatterns: ["*.cjs"],
+};
+
+module.exports = config;
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className="bg-red-400 h-40 w-40">Sudoku App!</div>;
+}
